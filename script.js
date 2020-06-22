18fb99b8a063d37acbafa797b0dc38f3712cf148
Alec Sands <sandsalecv@gmail.com>
2020-06-22 09:18:38 -0500

    basic arrow function within a filter method

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 console.log('js loaded');
 
-setInterval(() => {console.log('brain getting bigger');}, 1000);
+// setInterval(() => {console.log('brain getting bigger');}, 1000);
+
+let numbers = [1,2,3,4,5,6,7,8,9,0,1,2,3];
+
+let bigNumbers = numbers.filter(number => number > 5);
+
+console.log(bigNumbers);
